07d4c7f3aa0a57b3a1af22ee1c95829929742fc9
Shanelle Dejito <156997093+shanellegwapa123@users.noreply.github.com>
2024-01-25 16:28:27 +0800

    DEJITO_ACTIVITY_II.cpp

---
+++ Activities/Activity II/DEJITO_ACTIVITY_II.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1; 
+    int num2;
+
+    cout << "Enter first number:";
+    cin >> num1;
+    cout << "Enter second number:";
+    cin >> num2;
+
+    int sum = num1 + num2;
+    int diff = num1 - num2;
+    int prod = num1 * num2; 
+
+    cout << "sum = " << sum <<endl;
+    cout << "diff = " << sum <<endl;
+    cout << "prod = " << sum <<endl;
+}  
