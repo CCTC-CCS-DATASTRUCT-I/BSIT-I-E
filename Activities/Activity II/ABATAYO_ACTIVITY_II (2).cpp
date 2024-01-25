e2c1bcde721be358687009d28e92133d14f7e897
Abatayo Jonel C <156998123+JONELABATAYO@users.noreply.github.com>
2024-01-25 16:33:56 +0800

    Add files via upload

---
+++ Activities/Activity II/ABATAYO_ACTIVITY_II (2).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int z1;
+    int y2;
+
+    cout << "Enter first number:";
+    cin >> z1;
+    cout << "Enter second number:";
+    cin >> y2;
+
+  int sum = z1 + y2;
+  int diff =  z1 - y2;
+  int prod = z1 * y2;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Difference = " << diff << endl;
+    cout << "Product = " << prod << endl;
+
+}
