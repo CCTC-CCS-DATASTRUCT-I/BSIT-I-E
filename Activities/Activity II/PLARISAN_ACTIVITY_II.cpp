851341d6037090545ec1f937b9e2082f6a3c5d72
Jay P. Plarisan <156997011+BSITjay@users.noreply.github.com>
2024-01-25 16:25:01 +0800

    Add files via upload
    mao ni ako sir kanang akoa gyud ba hehe

---
+++ Activities/Activity II/PLARISAN_ACTIVITY_II.cpp
@@ -0,0 +1,20 @@
+#include <iostream>\
+
+using namespace std;
+int main(){
+    int y;
+    int z;
+    cout << " Enter first number:";
+    cin >> y;
+    cout << "Enter second number";
+    cin >> z;
+    int sum = y+z;
+    int diff = y-z;
+    int quotient = y*z;
+    cout << "sum =" << sum << endl;
+    cout << "diff =" << diff << endl;
+    cout << "quotient =" << quotient;
+
+
+
+}
