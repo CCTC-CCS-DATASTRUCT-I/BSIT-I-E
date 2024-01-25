fed6d9c92bf628ec8cb500ca02bf4f9c1fac7688
Mark R. Bandolon <156996757+mrbean147@users.noreply.github.com>
2024-01-25 16:10:54 +0800

    BANDOLON_ACTIVITY_II

---
+++ Activities/Activity II/BANDOLON_ACTIVITY_II.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+   int sum1;
+   int sum2;
+
+   cout << "Enter Your 1st Number: ";
+   cin >> sum1;
+   cout << "Enter Your 2nd Number: ";
+   cin >> sum2;
+
+   int sum = sum1 + sum2;
+
+   cout << "Your Output: " << sum << endl;
+
+}
