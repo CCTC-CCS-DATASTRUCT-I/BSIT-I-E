3a8bc98e39a0068362172700a82f832897efeb5c
CLIFTONWAYKLARO <157685815+CLIFTONWAYKLARO@users.noreply.github.com>
2024-01-25 16:27:36 +0800

    Add files via upload

---
+++ Activities/Activity II/TAMPUS_ACTIVITY_II.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1;
+    int num2;
+
+    cout << |"enter the first number:";
+    cin << num1;
+    
+    cout << "enter  the second number:";
+    cin << num2;
+
+    int sum = num1 + num2;
+    int diff =num1 - num2;
+    int prod = num1 * num2;
+
+    cout << "sum=" << sum <<endl;
+    cout << "diff=" << diff <<endl;
+    cout << "prod=" << prod <<endl;
+}
