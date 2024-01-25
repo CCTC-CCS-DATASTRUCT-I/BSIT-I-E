94483331b5e0d3bf91db2ecde90c7bbe967b2351
Jake Christian Javello <156996977+jakeeeeeeeeeeeeeeeee@users.noreply.github.com>
2024-01-25 16:12:41 +0800

    JAVELLO_ACTIVITYP_II

---
+++ Activities/Activity II/JAVELLO_ACTIVITY_II.cpp
@@ -0,0 +1,24 @@
+
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+int numb1;
+int numb2;
+
+cout << "Enter first number:";
+cin >> numb1;
+cout << "Enter second number: ";
+cin >> numb2; 
+
+int sum = numb1 + numb2;
+int diff = numb1 - numb2;
+int prod = numb1 * numb2;
+
+cout << "Sum =" << sum << endl;
+cout << "Diffrence = " << diff << endl;
+cout << "Product =" << prod << endl;
+}
+
+
