966be46ebc0ee67391381fd39978a3d957eba301
Medallo S. Reynaldo Jr <156997504+medallo272004@users.noreply.github.com>
2024-01-25 16:25:26 +0800

    Add files via upload

---
+++ Activities/Activity II/Medallo_ACTIVITY_II.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a;
+    int b;
+    cout << "Enter first number" << endl;
+    cin >> a;
+    cout << "Enter your second number" <<endl;
+    cin >> b;
+    
+
+
+}
