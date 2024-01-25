625d58005faa1e8ef440ccc7d7938cb9524694a6
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-01-25 16:24:58 +0800

    Add files via upload

---
+++ Activities/Activity II/LIBRADO_ACTIVITY_II.cpp
@@ -0,0 +1,20 @@
+// Your First C++ Program
+
+#include <iostream>
+using namespace std;
+int main() {
+int x, y;
+   cout << "Enter your first number: ";
+   cin >> x;
+   cout << "Enter your second number: ";
+   cin >> y;
+   int sum = x + y;
+   int diff = x - y;
+   int prod = x * y;
+
+   cout << "Sum is: " << sum << endl;
+   cout << "Diff is: " << sum << endl;
+   cout << "Prod is: " << sum << endl;
+  
+  
+}
