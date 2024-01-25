bffdac617dbec4b4edb7fdb0a9ea0af596796bc9
Dave Diaz <156997121+DhaboyDiaz@users.noreply.github.com>
2024-01-25 16:25:00 +0800

    Add files via upload

---
+++ Activities/Activity II/DIAZ_ACTIVITY_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int num1, num2;
+ cout << "Enter your Number: ";
+ cin >> x;
+ cout << "Enter your second Number: ";
+ cin >> y;
+ sum = x + y;
+ cout << "Sum is: " << sum;
+
+}
