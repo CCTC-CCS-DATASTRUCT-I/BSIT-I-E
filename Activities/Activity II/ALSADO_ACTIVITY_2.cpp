fbd297cdc66f3012860fbc1df61265bb954d4f8a
Vanz Rey Alsado <156997230+vanzrey@users.noreply.github.com>
2024-01-25 16:25:00 +0800

    Add files via upload

---
+++ Activities/Activity II/ALSADO_ACTIVITY_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int num1;
+    int num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    int sum = num1 + num2;
+    int sub = num1 - num2;
+    int mul = num1 * num2;
+
+    cout << "Sum = " << sum <<endl;
+    cout << "Sub = " << sub <<endl;
+    cout << "Mul = " << mul <<endl;
+
+    return 0;
+}
