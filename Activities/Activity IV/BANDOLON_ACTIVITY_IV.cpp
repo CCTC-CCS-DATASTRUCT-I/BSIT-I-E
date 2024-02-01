9739f19b19f3098ecf0445174c89a6a55be2112c
Mark R. Bandolon <156996757+mrbean147@users.noreply.github.com>
2024-02-01 15:27:56 +0800

    Add files via upload

---
+++ Activities/Activity IV/BANDOLON_ACTIVITY_IV.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+    using namespace std;
+
+int main() {
+    int Coffee;
+    cout << "Please Select Your Choice: 1, 2, 3, & 4."
+
+    cin >> coffee;
+        switch(coffee) {
+        case 1:
+        cout << "Espressso";
+        break;
+        case 2:
+        cout << "Americano";
+        break;
+        case 3:
+        cout << "Cappuccino";
+        break;
+        case 4:
+        cout << "Latte";
+        break;
+        default;
+        cout << "Invalid Choice";
+    }
+}
