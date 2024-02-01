e309023c7fea06154e7741d690e89c20d2623266
Marc Lyster Canillo <156999510+MarcHAHAHA@users.noreply.github.com>
2024-02-01 17:03:43 +0800

    Add files via upload

---
+++ Activities/Activity IV/CANILLO_ACTIVITY_IV.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int coffee;
+    cout << "Choose your coffee (1 Espresso, 2 Americano, 3 Cappuccino, 4 Latte): ";
+
+    cin >> coffee;
+
+    switch (coffee) {
+        case 1:
+            cout << "Espresso";
+            break;
+        case 2:
+            cout << "Americano";
+            break;
+        case 3:
+            cout << "Cappuccino";
+            break;
+        case 4:
+            cout << "Latte";
+            break;
+        default:
+            cout << "Invalid choice";
+            break;
+    }
+
+}
