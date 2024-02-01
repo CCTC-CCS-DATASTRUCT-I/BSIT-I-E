db70247f6a76b83c194431c0a8e312868896ea2d
Shanelle Dejito <156997093+shanellegwapa123@users.noreply.github.com>
2024-02-01 15:30:50 +0800

    EJITO_ACTIVITY_IV.cpp

---
+++ Activities/Activity IV/DEJITO_ACTIVITY_IV.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int Coffee;
+    cout<< "Espresso,Americano,Cappuccino,Latte";
+    cin>> Coffee;
+
+    switch(Coffee){
+    case 1:
+    cout<<"Espresso";
+    break;
+    case 2:"Americano";
+    break;
+    case 3:"Cappuccino";
+    break;
+    case 4:"Latte";
+    break;
+
+
+}
+   
+
+    }
+       
