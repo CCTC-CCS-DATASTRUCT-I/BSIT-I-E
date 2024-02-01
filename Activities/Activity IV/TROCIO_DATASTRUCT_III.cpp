68a67205dd019d52a62ec3675fdcec3c12e743c4
edmundtrocio <157685632+edmundtrocio@users.noreply.github.com>
2024-02-01 15:44:56 +0800

    Update TROCIO_DATASTRUCT_III.cpp

---
+++ Activities/Activity IV/TROCIO_DATASTRUCT_III.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int kape;
+    cout<< "pili ug kape (1.Esepresso 2.Americano 3.Cappuccino 4.Latte)"
+    cin>> kape
+    
+    switch(choice) {
+        case 1;
+        cout<< "Espresso";
+        break;
+        case 2;
+        cout<< "Americano";
+        case 3;
+        cout<< "Cappucino"
+        case 4;
+        cout<< "Latte"
+
+    }
+
+    }
