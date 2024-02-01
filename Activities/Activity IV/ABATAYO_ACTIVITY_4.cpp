293f749e823c909e0a0698b0fde8d5471a3afee4
Abatayo Jonel C <156998123+JONELABATAYO@users.noreply.github.com>
2024-02-01 15:29:03 +0800

    Add files via upload

---
+++ Activities/Activity IV/ABATAYO_ACTIVITY_4.cpp
@@ -0,0 +1,27 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+     int coffee;
+     cout << " choice your coffee (Espreso, Americano, Cappuccino, Latte:);
+
+     cin >>
+      switch(coffee) {
+
+     case 1;
+     cout << "Espreso";
+     break;
+     case 2;
+     cout << "Americano";
+     break;
+     case 3;
+     cout << "Cappuccino";
+     break;
+     cout << "Latte";
+     break;
+     default;
+     cout << "invalid choice";
+     
+    }
+    
+}
