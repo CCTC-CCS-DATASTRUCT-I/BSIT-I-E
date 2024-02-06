9017836c79e8b4fd5702e1011397f909469b1394
AGUANTA LEE <157687146+Aguanta1@users.noreply.github.com>
2024-02-06 15:52:04 +0800

    Add files via upload

---
+++ Activities/Activity V/AGUANTA_ACTIVITY 5 .cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enter age: ";
+    cin >> age;
+
+    if (age >= 0 && age <= 3){
+        cout << ("free ticket");
+       
+    else if (age >= 4 && age <= 6){
+        cout << ("discount ticket");
+
+        else if (age >= 7){
+            cout << ("normal");
+        }
+    
+    }
+    
+    }
+ }
+Write to Marc Lyster Gomez Canillo
+
