a7a0b4aa8e2f65b2be0365ba7a5d40f6c92b2e91
Mark R. Bandolon <156996757+mrbean147@users.noreply.github.com>
2024-02-06 15:37:30 +0800

    BANDOLON_ACVITIY_V

---
+++ Activities/Activity V/BANDOLON_ACTIVITY_5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int age;
+    
+    cout << "Free: Age 0-3 get free tickets." << endl;
+    cout << "Discounted: Ages 4-6 get discounted tickets." << endl;
+    cout << "Normal: Ages 7+ get a normal ticket." << endl;
+    cout << "Enter your age here: ";
+
+    cin >> age;
+
+    if (age >= 0 && age <=3) {
+        cout << "You received a free ticket!";
+    } else if (age <= 4 && age <=6) {
+       cout << "You received discounted ticket!";
+    } else if (age >= 7) {  
+       cout << "You received normal ticket!";
+    }
+}
