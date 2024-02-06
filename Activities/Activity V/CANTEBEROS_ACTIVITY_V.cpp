7ec56ad708553d51e45b3dfa753b802d1f2cefe2
Kimberly C. Canteberos <156997125+kmbrlycntbrs@users.noreply.github.com>
2024-02-06 15:36:25 +0800

    Add files via upload

---
+++ Activities/Activity V/CANTEBEROS_ACTIVITY_V.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+    int age;
+    cout << "Enter age: ";
+
+    cin >> age;
+
+    if (age <= 3){
+        cout << ("Free Ticket");
+        break;
+
+    else(age <= 6){
+        cout << ("Discounted");
+
+    if else (age >= 7){
+        cout << ("Normal Ticket");
+    }
+    }
+    }
+ }
