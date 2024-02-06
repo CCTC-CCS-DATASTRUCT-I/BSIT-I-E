59e4d9e832bd19b255b60dee2459cbe4f8fb1142
Ricky Ponce <156997773+rickyboyyyyy@users.noreply.github.com>
2024-02-06 15:37:22 +0800

    Add files via upload

---
+++ Activities/Activity V/RICKY P. ACTIVITY_V.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+    cout<< "Enter Age";
+    cin>> age;
+
+    switch (age){
+        case 0:
+        cout << "Free ticket" << endl;
+        break;
+        case 1:
+        cout << "Free ticket" << endl;
+        break;
+        case 2:
+        cout << "Free ticket" << endl;
+        break;
+        case 3:
+        cout << "Free ticket" << endl;
+        break;
+        case 4:
+        cout << "Discounted ticket" << endl;
+        break;
+        case 5:
+        cout << "Discounted ticket" << endl;
+        break;
+        case 6:
+        cout << "Discounted ticket" << endl;
+        break;
+        case 7:
+        cout << "Normal ticket" << endl;
+        break;
+    }
+
+
+}
+
+
+
+
