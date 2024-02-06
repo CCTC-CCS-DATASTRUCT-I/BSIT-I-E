70fc978fbea26779cd870f13d6f688bb537fe785
Medallo S. Reynaldo Jr <156997504+medallo272004@users.noreply.github.com>
2024-02-06 15:37:55 +0800

    Add files via upload

---
+++ Activities/Activity V/MEDALLO_ACTIVITY_V.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age;
+    cout << "Enter age";
+    cin >> age;
+    
+    switch (age){
+        case 0:
+        cout << "Free ticket" << endl;
+        break;
+        case 1:
+        cout << "Free ticket" << endl;
+        break;
+        case 2:
+        cout << "Free ticket" << endl;
+        break;
+        case 3:
+        cout << "Free ticket" << endl;
+        break;
+        case 4:
+        cout << "Free ticket" << endl;
+        break;
+        case 5:
+        cout << "Discounted ticket" << endl;    
+        break;
+        case 6:
+        cout << "Discounted ticket" << endl;
+        break;
+        case 7:
+        cout << "Normal ticket" << endl;
+        break;
+        case 8:
+        cout << "Normal ticket" << endl;
+        break;
+           
+        }
+    
+}
