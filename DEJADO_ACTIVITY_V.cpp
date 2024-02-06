c9235b29a2f7923a71e7958bb20f3f993ba4b02f
Cris Jeano Dejado <156996907+jeanogwapo@users.noreply.github.com>
2024-02-06 15:43:17 +0800

    DEJADO_ACTIVITY_V.cpp

---
+++ DEJADO_ACTIVITY_V.cpp
@@ -0,0 +1,26 @@
+#include <iosstream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<< "Enter your age";
+    cin>>age;
+
+if(age >=0 && age <= 3){
+    cout<<"FREE";
+
+}else if(age >= 4 && <= 6){
+    cout<< "DISCOUNTED";
+}
+}else if(age >=7); {
+   cout << "Normal" << endl;
+} else {
+  cout << "Invalid age. Please enter a non negative age." << endl;
+}
+
+}
+    
+
+
+    
+
