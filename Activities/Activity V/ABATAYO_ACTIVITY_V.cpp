6185fe7f164cb540b8f8fd7c54d7595d2c0120f8
Abatayo Jonel C <156998123+JONELABATAYO@users.noreply.github.com>
2024-02-06 15:47:36 +0800

    Add files via upload

---
+++ Activities/Activity V/ABATAYO_ACTIVITY_V.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace;
+
+int main() {
+    int age;
+    cout<<"Enter age:";
+    cin>>age;
+     
+     if(age >= 7) {
+       cout << "Get normal ticket";
+     
+     }
+     else if (age <= 6 && age >= 4); {
+       cout << "get discounted tickets"
+
+    else if(age >= 0 && <= 3);
+    cout << "get free tickets";
+    
+    
+    }
+}
