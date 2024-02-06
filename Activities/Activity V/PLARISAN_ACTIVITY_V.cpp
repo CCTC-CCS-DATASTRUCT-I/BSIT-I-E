aaa78fa336242c15bb75b0efe00987d65918769d
Jay P. Plarisan <156997011+BSITjay@users.noreply.github.com>
2024-02-06 15:36:19 +0800

    Add files via upload

---
+++ Activities/Activity V/PLARISAN_ACTIVITY_V.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enter age: ";
+    cin >> age;
+
+    switch(age){
+        case 0:
+        cout << "Free";
+        break;
+
+        case 1:
+        cout << "Free"; 
+        break;
+
+        case 2:
+        cout << "Free";
+        break;
+
+        case 3:
+        cout << "Free";
+        break;
+
+        case 4:
+        cout << "Discounted";
+        break;
+
+        case 5:
+        cout << "Discounted";
+        break;
+
+        case 6:
+        cout << "Discounted";
+        break;
+
+        case 7:
+        cout << "Normal";  
+        break;
+
+        case 8:
+        cout <<"Normal";
+
+    }
+}
