ec91bd1557944c1500909b0a28d0a442d179f782
Jake Christian Javello <156996977+jakeeeeeeeeeeeeeeeee@users.noreply.github.com>
2024-02-06 15:42:09 +0800

    Add files via upload

---
+++ Activities/Activity V/JAVELLO_ACTIVITY_V.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<< "Enter age: ";
+    cin>>age;
+
+    if(age>=0 && age <=3){
+        cout<<"FREE";
+        
+    }else if (age>= 4 && age <=6){
+        cout<<"Get discount";
+
+        }else(age>= 0 && age <=3);
+        cout <<"NORMAL TICKET";
+    }
