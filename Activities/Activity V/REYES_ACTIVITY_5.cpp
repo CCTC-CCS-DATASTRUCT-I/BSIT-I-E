8a76bb9cdb3550e888c8d23dc428fb64042e8de5
Doven <156999719+Ventaejin@users.noreply.github.com>
2024-02-06 15:44:22 +0800

    Add files via upload

---
+++ Activities/Activity V/REYES_ACTIVITY_5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int age;
+   cout << "Free: 0-3" << endl;
+   cout << "Discounted: 4-6" << endl;
+   cout << "Normal: 7+" << endl;
+    cout << "Enter your age:";
+    cin >> age;
+    if (age <= 3) {
+        cout << "You are free!";
+    } else if (age >=4 && age <=6) {
+        cout << "You are discounted!";
+    } else if (age >= 7) { 
+        cout << "You are at a normal age.";
+    }
+   
+}
