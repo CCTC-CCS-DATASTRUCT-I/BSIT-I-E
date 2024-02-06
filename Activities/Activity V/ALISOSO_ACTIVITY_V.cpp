7bfb02c3d969e2ee4f812fdc5d07760042f78e61
Janber Alisoso <156997592+janber0802@users.noreply.github.com>
2024-02-06 15:41:56 +0800

    Add files via upload

---
+++ Activities/Activity V/ALISOSO_ACTIVITY_V.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter age:";
+    cin>>age;
+
+    if(age>= 7){
+        cout<<"get normal ticket";
+    }
+     else if(age>= 4 && age<= 6){
+        cout<<"get discounted tickets";
+    }
+    else if(age>= 0 && age <= 3);
+    cout<<"get free tickets";
+
+    }
