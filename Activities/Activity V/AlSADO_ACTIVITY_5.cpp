60bc174d8caf2557372e163536f92abd5eba53cc
Vanz Rey Alsado <156997230+vanzrey@users.noreply.github.com>
2024-02-06 15:41:27 +0800

    Add files via upload

---
+++ Activities/Activity V/AlSADO_ACTIVITY_5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ using namespace std;
+ 
+ int main (){
+    int age;
+ cout << "Enter the age of the customer: ";
+ cin >> age;
+
+    if (age >= 0 && age <= 3){
+    cout << "Free" << endl;
+  } else if (age >= 4 && age <=6){
+    cout << "Discounted" << endl;
+  } else if (age >= 7) {
+    cout << "Normal" << endl;
+  } else {
+    cout << "Invalid age. Please enter a non-negative age." << endl;
+  }
+ }
+
