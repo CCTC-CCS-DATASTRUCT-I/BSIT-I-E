9cbab86cb29ed1d6f583bbb65773c4d02c82b591
Rieda Mae Ygay <156997539+RiedaYgay@users.noreply.github.com>
2024-02-08 15:40:00 +0800

    Add files via upload

---
+++ Activities/Activity VI/YGAY_ACTIVITY_VI.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int math 
+    cout<< "do you want to start the transaction?";
+    cin>> amount;
+    int price1 = 0;
+    int price2 = 0;
+
+    while(price2 <= amount){
+        price1+=price2;
+        price2++;
+
+    }    
+    cout<<price1;
+     
+}
