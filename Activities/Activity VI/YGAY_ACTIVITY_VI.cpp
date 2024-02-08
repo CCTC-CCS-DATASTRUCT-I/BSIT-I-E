35514a4d8ca9d428fe28c4ddfa8f0fb3bef8f658
Rieda Mae Ygay <156997539+RiedaYgay@users.noreply.github.com>
2024-02-08 15:49:14 +0800

    Add files via upload

--- Activities/Activity VI/YGAY_ACTIVITY_VI.cpp
+++ Activities/Activity VI/YGAY_ACTIVITY_VI.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int math 
-    cout<< "do you want to start the transaction?";
-    cin>> amount;
+int main() {
+    int math;
+    cout << "Do you want to start the transaction?"; // Type yes or no and enter
+    cin >> amount; // Get user input from the keyboard
     int price1 = 0;
     int price2 = 0;
 
     while(price2 <= amount){
         price1+=price2;
         price2++;
-
-    }    
+    }
     cout<<price1;
-     
+
 }
