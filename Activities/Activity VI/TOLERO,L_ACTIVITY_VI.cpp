78f5dee1e3db56111bcc051a9221bd3f5ddbc66f
Lloyd Tres Tolero <90170882+waragud@users.noreply.github.com>
2024-02-08 15:50:20 +0800

    Add files via upload

---
+++ Activities/Activity VI/TOLERO,L_ACTIVITY_VI.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string answer ;
+    double total;
+    
+    cout << "Do you want to start the transaction? (yes/no) ";
+    cin >> answer;
+
+
+    while(answer == "yes"){
+
+        double price;
+
+        cout << "Enter amount: ";
+        cin >> price;
+
+        total += price;
+
+        cout << "Do you want to continue? (yes/no) ";
+        cin >> answer;
+
+
+    }
+    cout << "The total amount is: " << total << endl;
+
+}
