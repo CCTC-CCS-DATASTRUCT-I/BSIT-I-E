a29d71683285acaf558e75a5720079aaa1943062
Sonnyboy C. Curayag <156996922+Sonnyboy29@users.noreply.github.com>
2024-05-09 14:46:34 +0800

    Update CURAYAG_ACTIVITY_V.cpp

--- Activities/Activity V/CURAYAG_ACTIVITY_V.cpp
+++ Activities/Activity V/CURAYAG_ACTIVITY_V.cpp
@@ -1,41 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
-  int age;
-  cout << "Enter age: ";
-  cin >> age;
-
-  switch (age){
-    case 0:
-    cout << "Free ticket" << endl;
-    break;
-    case 1:
-    cout << "Free ticket" << endl;
-    break;
-    case 2:
-    cout << "Free ticket" << endl;
-    break;
-    case 3:
-    cout << "Free ticket" << endl;
-    break;
-    case 4:
-    cout << "Discounted ticket" << endl;
-    break;
-    case 5:
-    cout << "Discounted ticket" << endl;
-    break;
-    case 6:
-    cout << "Discounted ticket" << endl;
-    break;
-    case 7:
-    cout << "Normal ticket" << endl;
-    break;
-    case 8:
-    cout << "Normal ticket" << endl;
-    break;
-  }
-  
-
-}
+    int main(){
+        int age;
+        cout << "Enter your age: ";
+        cin >> age;
+        
+    if(age >= 0 && age <= 3){
+        cout <<"Free ticket" << endl;
+    }   
+    else if(age >= 4 && age <= 6){
+        cout <<"Discounted ticket" << endl;
+    }   
+    else if(age >= 7){
+        cout <<"Normal ticket" << endl;
+    }
+   }
