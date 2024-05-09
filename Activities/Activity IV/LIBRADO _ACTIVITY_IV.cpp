675afec62261dc6cba761846ae3b6104bb22fc8d
Loriano Librado Jr <156996947+raprap111@users.noreply.github.com>
2024-05-09 14:38:29 +0800

    Update LIBRADO _ACTIVITY_IV.cpp

--- Activities/Activity IV/LIBRADO _ACTIVITY_IV.cpp
+++ Activities/Activity IV/LIBRADO _ACTIVITY_IV.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 using namespace std;
-int main () {
-    
+
+int main() {
     int coffee;
-    cout << "choose your coffee (1. espreso, 2. americano, 3. cappuccino, 4. latte): ";
+    cout << "Choose your coffee (1. espresso, 2. americano, 3. cappuccino, 4. latte): ";
     cin >> coffee;
+
     switch (coffee) {
-        case '1':
-        cout << "espreso";
-        break;
-        case '2':
-        cout << "americano";
-        break;
-        case '3':
-        cout << "cappuccino";
-        break;
-        case '4':
-        cout << "latte";
-        break;
+        case 1:
+            cout << "espresso";
+            break;
+        case 2:
+            cout << "americano";
+            break;
+        case 3:
+            cout << "cappuccino";
+            break;
+        case 4:
+            cout << "latte";
+            break;
         default:
-        cout << "invalid choice";
+            cout << "Invalid choice";
     }
 
+  
 }
