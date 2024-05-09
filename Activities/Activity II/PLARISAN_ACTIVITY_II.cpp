a052b273954c08a1b4f6850ebae4feab7bf1c2f9
Jay P. Plarisan <156997011+BSITjay@users.noreply.github.com>
2024-05-09 14:55:00 +0800

    Update PLARISAN_ACTIVITY_II.cpp

--- Activities/Activity II/PLARISAN_ACTIVITY_II.cpp
+++ Activities/Activity II/PLARISAN_ACTIVITY_II.cpp
@@ -1,20 +1,20 @@
-#include <iostream>\
+#include <iostream>
 
 using namespace std;
-int main(){
+
+int main() {
     int y;
     int z;
-    cout << " Enter first number:";
+    cout << "Enter first number: ";
     cin >> y;
-    cout << "Enter second number";
+    cout << "Enter second number: ";
     cin >> z;
-    int sum = y+z;
-    int diff = y-z;
-    int quotient = y*z;
-    cout << "sum =" << sum << endl;
-    cout << "diff =" << diff << endl;
-    cout << "quotient =" << quotient;
+    int sum = y + z;
+    int diff = y - z;
+    int quotient = y / z; // Corrected division operation
+    cout << "sum = " << sum << endl;
+    cout << "diff = " << diff << endl;
+    cout << "quotient = " << quotient << endl;
 
-
-
+ 
 }
