b8b94a8554cb04ac89919359c37de8e6e24eb1fb
AGUANTA LEE <157687146+Aguanta1@users.noreply.github.com>
2024-05-09 08:40:10 +0800

    Update AGUANTA_ACTIVITY 5 .cpp

--- Activities/Activity V/AGUANTA_ACTIVITY 5 .cpp
+++ Activities/Activity V/AGUANTA_ACTIVITY 5 .cpp
@@ -1,23 +1,22 @@
+
 #include <iostream>
 using namespace std;
 
-int main(){
+int main () {
     int age;
-    cout << "Enter age: ";
+    
+    cout << "Free: Age 0-3 get free tickets." << endl;
+    cout << "Discounted: Ages 4-6 get discounted tickets." << endl;
+    cout << "Normal: Ages 7+ get a normal ticket." << endl;
+    cout << "Enter your age here: ";
+
     cin >> age;
 
-    if (age >= 0 && age <= 3){
-        cout << ("free ticket");
-       
-    else if (age >= 4 && age <= 6){
-        cout << ("discount ticket");
-
-        else if (age >= 7){
-            cout << ("normal");
-        }
-    
+    if (age >= 0 && age <=3) {
+        cout << "You received a trial ticket!";
+    } else if (age <= 4 && age <=6) {
+       cout << "You received discounted ticket!";
+    } else if (age >= 7) {  
+       cout << "You received basic ticket!";
     }
-    
-    }
- }
-
+}
