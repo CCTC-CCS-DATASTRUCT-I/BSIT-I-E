7149709369951fc4ab08279e68e668525190eb07
Loren L. Rebias <156997161+lorenrebias@users.noreply.github.com>
2024-05-09 15:57:24 +0800

    Update REBIAS_ACTIVITY_V.cpp

--- Activities/Activity V/REBIAS_ACTIVITY_V.cpp
+++ Activities/Activity V/REBIAS_ACTIVITY_V.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+int main () {
     int age;
-    cout << "Enter age: ";
-
-    cin >> age;;
-    
-    if (age <= 3){
-        cout << ("Free Ticket");
-        break;
-
-    else (age <= 6) {
-        cout << ("Discounted");
-
-    if else (age >= 7){
-        cout << ("Normal Ticket");
-    }    
+   cout << "Free: 0-3" << endl;
+   cout << "Discounted: 4-6" << endl;
+   cout << "Normal: 7+" << endl;
+    cout << "Enter your age:";
+    cin >> age;
+    if (age <= 3) {
+        cout << "You are free!";
+    } else if (age >=4 && age <=6) {
+        cout << "You are discounted!";
+    } else if (age >= 7) { 
+        cout << "You are at a normal age.";
     }
-    }
+   
 }
